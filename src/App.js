4fa4adda0c12abf91c68a6d45c259de48cce55f5
github username <winter.lamon@gmail.com>
2018-02-21 14:10:02 -0500

    added API fetch to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import {} from "semantic-ui-react";
 import BirthdayForm from "./components/BirthdayForm";
 
 class App extends Component {
-  findPeopleByBirthday(birthday) {}
+  findPeopleByBirthday(birthday) {
+    return fetch(`http://localhost:3000/ivy-imdb/people/${birthday}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json"
+      },
+      body: JSON.stringify({ birthday })
+    })
+      .then(res => res.json())
+      .then(people => console.log(people));
+  }
 
   render() {
     return (
       <div className="App">
-        <BirthdayForm />
+        <BirthdayForm findPeopleByBirthday={this.findPeopleByBirthday} />
       </div>
     );
   }
